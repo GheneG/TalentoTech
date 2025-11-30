5bc94271beda1518c3e88438507f1959ef12dd58
GheneG <ghenesisguevara@hotmail.com>
2025-11-30 01:28:11 -0300

    Nueva sección Tienda, para proyecto final y empezar a agregar js

--- js/index.js
+++ js/index.js
@@ -1,34 +1,59 @@
-/* let num1 = prompt("Ingresa un número");
-let num2 = prompt("Ingresa otro número");
+    const productos = [
+        {id:10,
+        name: "Remera",
+        description: "Remera de algodón",
+        amount: 20
+        },
+        {id:20,
+        name: "Pantalón",
+        description: "Pantalón de mezclilla",
+        amount: 15
+        },
+        {id:30,
+        name: "Zapatos",
+        description: "Zapatos deportivos",
+        amount: 10
+        },
+        {id:40,
+        name: "Correa",
+        description: "Correa de cuero",
+        amount: 25},
+        {id:50,
+            name: "Gorra",
+            description: "Gorra de béisbol",
+            amount: 30
+        }
+    ];
 
-if (isNaN(num1) || isNaN(num2)) {
-    alert("Por favor ingresa números válidos");
-} else {
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-}
+let contenedor = document.getElementById("contenedor-productos");
+productos.forEach(function(producto) {
+    let card = document.createElement('article');
+    card.classList.add('card');
 
+    let productName = document.createElement('h3');
+    productName.textContent = producto.name;
 
-let suma = num1 + num2;
-let resta = num1 - num2;
-let multiplicacion = num1 * num2;
-let division = num1 / num2;
+    let productPrice = document.createElement('p');
+    productPrice.textContent = producto.amount;
 
-console.log("Suma= " + suma)
-console.log("Resta= " + resta)
-console.log("Multiplicación= " + multiplicacion)
-console.log("División= " + division) */
+    let productBoton = document.createElement('button');
+    productBoton.textContent = "Ver descripción";
 
-let nombre = prompt("Ingresa tu nombre");
-let edad = prompt("Ingresa tu edad");
+    let productDescription = document.createElement('p');
+    productDescription.textContent = producto.description;
+    productDescription.style.display = 'none';
 
-if (isNaN(edad)) {
-    alert("Por favor ingresa una edad válida");
-} else {
-    edad = parseInt(edad);
-    if (edad < 18) {
-        alert("Eres menor de edad");
-    } else {
-        alert("Eres mayor de edad");
-}
-}
+    card.appendChild(productName);
+    card.appendChild(productPrice);
+    card.appendChild(productBoton);
+    card.appendChild(productDescription);
+    contenedor.appendChild(card);
+
+    productBoton.addEventListener('click', function(event) {
+        if (productDescription.style.display === 'none') {
+            productDescription.style.display = 'block';
+        };
+    });
+    })
+
+    
